86920d07780552a6981d9ee8192808d74443a0cd
John Kim <joimkim@gmail.com>
2016-09-22 19:44:34 -0700

    clean up projcet add HeroSegment Component, HeroTitle Component

---
+++ components/Title/Title.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+import s from './styles.css';
+
+class Title extends React.Component {
+	render () {
+		return (
+			<div>title </div>
+		);
+	}
+}
+
+export default Title
