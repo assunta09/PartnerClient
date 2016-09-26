a75f4ef74039bdaf34d74f05987b32e8e89c9730
John Kim <joimkim@gmail.com>
2016-09-26 11:48:44 -0700

    Example working with PartnerAPI endpoint

--- src/Actions/actions.js
+++ src/Actions/actions.js
@@ -19,7 +19,6 @@ export function invalidateSubreddit(subreddit) {
 }
 
 export const REQUEST_POSTS = 'REQUEST_POSTS'
-
 export function requestPosts(subreddit) {
   return {
     type: REQUEST_POSTS,
@@ -28,12 +27,49 @@ export function requestPosts(subreddit) {
 }
 
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
-
 export function receivePosts(subreddit, json) {
   return {
     type: RECEIVE_POSTS,
     subreddit,
-    posts: json.data.children.map(child => child.data),
+    posts: json,
     receivedAt: Date.now()
+  }
+}
+
+// Meet our first thunk action creator!
+// Though its insides are different, you would use it just like any other action creator:
+// store.dispatch(fetchPosts('reactjs'))
+
+export function fetchPosts(subreddit) {
+
+  // Thunk middleware knows how to handle functions.
+  // It passes the dispatch method as an argument to the function,
+  // thus making it able to dispatch actions itself.
+
+  return function (dispatch) {
+
+    // First dispatch: the app state is updated to inform
+    // that the API call is starting.
+
+    dispatch(requestPosts(subreddit))
+
+    // The function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method.
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch(`https://partner-api.herokuapp.com/organisations/${subreddit}`)
+      .then(response => response.json())
+      .then(json =>
+
+        // We can dispatch many times!
+        // Here, we update the app state with the results of the API call.
+
+        dispatch(receivePosts(subreddit, json))
+      )
+
+      // In a real world app, you also want to
+      // catch any error in the network call.
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import App from './App';
 import HeroSegment from './Components/HeroSegment/HeroSegment.js'
 import DoughnutChart from './Components/GraphContainer/DoughnutChart.js';
 import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import ExpenseContainer from './Components/ExpenseSegment/ExpenseContainer.js';
 
+import { selectSubreddit, fetchPosts } from './Actions/actions.js'
 import {rootReducers} from './Reducers/reducers.js';
 
 import './index.css';
@@ -17,7 +19,15 @@ const store = createStore(
   combineReducers({
     ...rootReducers,
     routing: routerReducer
-  })
+  }),
+  applyMiddleware(
+  	thunkMiddleware,
+  )
+)
+
+store.dispatch(selectSubreddit('1'))
+store.dispatch(fetchPosts('1')).then(() =>
+  console.log(store.getState())
 )
 
 // Create an enhanced history that syncs navigation events with the store
