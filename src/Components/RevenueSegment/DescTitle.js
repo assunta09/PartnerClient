993485af76019ceea006568a505687393257e6b1
Connor Ray <connor.ray.cb@gmail.com>
2016-09-26 11:25:57 -0700

    Creates Revenue folder

---
+++ src/Components/RevenueSegment/DescTitle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+var DescTitle = React.createClass({
+
+	render () {
+
+		return (
+			<div className='DescTitle'>
+        <h2 className='DescriptionTitle'>Reveune</h2>
+			</div>
+		);
+	}
+});
+
+
+export default DescTitle;
