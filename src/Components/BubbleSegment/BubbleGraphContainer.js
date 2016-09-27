8cc950cb426c0e837d77114f37be64da61821a6c
Blake Wills <blake.wills525@gmail.com>
2016-09-27 11:48:29 -0700

    Update canvas for bubble segment to fit area; bubble chart construction begins now

--- src/Components/BubbleSegment/BubbleGraphContainer.js
+++ src/Components/BubbleSegment/BubbleGraphContainer.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import PolarArea from '../GraphContainer/PolarAreaChart.js';
+
 
 var GraphContainer = React.createClass({
 
   render () {
 
     return (
-      <div className='GraphContainer'>
-        <PolarArea reports={this.props.reports}/>
+      <div className='BubbleGraphContainer'>
       </div>
     );
   }
