a644f1a6e6494f4bcdba375eb66d43fce9244a96
Blake Wills <blake.wills525@gmail.com>
2016-09-27 10:11:51 -0700

    Decided to use article segment as test for bubble because it's readily visible

--- src/Components/ArticleSegment/ArticleSegment.js
+++ src/Components/ArticleSegment/ArticleSegment.js
@@ -7,6 +7,7 @@ var ArticleSegment = React.createClass({
   render() {
     return (
       <div className='ArticleSegment'>
+      <h1>This is the test bubble segment</h1>
       </div>
     );
   }
