224a3f0f7ea94f084ec966f74819e96f3f4d5e9a
Blake Wills <blake.wills525@gmail.com>
2016-09-27 10:08:59 -0700

    Modify bubble container with the same layout and sectioning as articles container

---
+++ src/Components/BubbleSegment/BubbleContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+var BubbleContainer = React.createClass({
+
+  render() {
+    return (
+      <div className='BubbleContainer'>
+      </div>
+    );
+  }
+});
+
+export default BubbleContainer;
---
+++ src/Components/BubbleSegment/BubbleSegment.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.css';
+import BubbleContainer from '../BubbleSegment/BubbleContainer.js'
+
+var BubbleSegment = React.createClass({
+
+  render() {
+    return (
+      <div className='BubbleSegment'>
+      </div>
+    );
+  }
+});
+
+export default BubbleSegment;
