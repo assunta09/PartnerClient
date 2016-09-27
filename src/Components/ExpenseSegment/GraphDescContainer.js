d47617df5186b6e78073d233ed9561f558fd019a
John Kim <joimkim@gmail.com>
2016-09-27 11:58:37 -0700

    implement scrollreveal

--- src/Components/ExpenseSegment/GraphDescContainer.js
+++ src/Components/ExpenseSegment/GraphDescContainer.js
@@ -3,17 +3,15 @@ import DescTitle from './DescTitleContainer.js'
 import Graph from './ExpenseGraphContainer.js'
 
 var GraphDescContainer = React.createClass({
-
+  
 	render () {
-
 		return (
 			<div className='GraphDescContainer'>
-        <DescTitle reports={this.props.reports}/>
-        <Graph reports={this.props.reports}/>
+        	<DescTitle reports={this.props.reports}/>
+        	<Graph reports={this.props.reports}/>
 			</div>
 		);
 	}
 });
-
 
 export default GraphDescContainer;
--- src/Components/RevenueSegment/GraphDescContainer.js
+++ src/Components/RevenueSegment/GraphDescContainer.js
@@ -1,15 +1,47 @@
 import React from 'react'
 import DescTitle from './DescTitleContainer.js'
 import Graph from './RevenueGraphContainer.js'
+import ScrollReveal from 'scrollreveal'
+
+
 
 var GraphDescContainer = React.createClass({
+		
+	componentDidMount: function () {
+    const config = {
+      origin: 'right',
+      duration: 500,
+      delay: 100,
+      distance: '100px',
+      scale: 1,
+      easing: 'ease',
+      reset: true,
+    }
+
+    const config2 = {
+      origin: 'left',
+      duration: 500,
+      delay: 100,
+      distance: '100px',
+      scale: 1,
+      easing: 'ease',
+      reset: true,
+    }
+    var sr = ScrollReveal();
+    sr.reveal(this.refs.box1, config)
+    sr.reveal(this.refs.box2, config2)
+  },
 
 	render () {
 
 		return (
 			<div className='GraphDescContainer1'>
-        <DescTitle />
-        <Graph />
+        <div ref='box1'>
+        	<DescTitle/>
+        </div>
+        <div ref='box2'>
+        	<Graph/>
+        </div>
 			</div>
 		);
 	}
