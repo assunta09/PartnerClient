d08c816a19b444293d5ee52141c713f084348d90
Connor Ray <connor.ray.cb@gmail.com>
2016-09-26 18:44:42 -0700

    Creates typeahead search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,24 +1,42 @@
 import React from 'react';
 import './styles.css';
+var Typeahead = require('react-typeahead').Typeahead;
 
 var SearchBar = React.createClass({
 	getInitialState: function() {
-		return {value: ''};
+		return {
+			value: '',
+			chartData: [1]
+		};
 	},
 
 	handleChange: function(event) {
 		this.setState({value: event.target.value});
 	},
 
+	componentWillMount: function() {
+    return fetch('https://partner-api.herokuapp.com/organisations')
+      .then((response) => response.json())
+      .then((Data) => {
+        console.log(Data)
+			var list = (Data.organisations)
+        this.setState({
+          chartData: Object.keys(list)
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+
 	render () {
 		return (
 			<div>
-				<input
-					type="text"
-					value={this.state.value}
-					onChange={this.handleChange}
-					className={'SearchBarContainer'}
-				/>
+			{console.log(this.state.chartData)}
+				<Typeahead className={'SearchBarContainer'}
+			    options={this.state.chartData}
+			    maxVisible={5}
+			  />
 				{console.log(this.state.value)}
 			</div>
 		);
