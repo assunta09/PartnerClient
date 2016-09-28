107d2741527476a60c3eb9ffef461b67ab47b6ce
Connor Ray <connor.ray.cb@gmail.com>
2016-09-27 20:24:58 -0700

    Restructures hero page, adds search box and mission segment

--- src/Components/HeroSegment/HeroSegment.js
+++ src/Components/HeroSegment/HeroSegment.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import HeroTitle from './HeroTitle.js'
-import SearchBar from '../SearchBar/SearchBar.js';
 import DescriptionContainer from '../DescriptionContainer/DescriptionContainer.js'
 import './styles.css';
 
 var HeroSegment = React.createClass({
   render() {
     return (
-    	<div className="HeroBackground"> 
+    	<div className="HeroBackground">
+        <HeroTitle/>
 	    	<div className="HeroContainer">
-	    		<HeroTitle/>
-          <SearchBar/>
+          <h3>Our Mission</h3>
           <DescriptionContainer width={'80%'} height={100} content={
             `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys
-             standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to 
-             make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
+             standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+             make a type specimen book. It has survived not only five centuries, but also the leap into electronic
              typesetting, remaining essentially`
           }/>
         {this.props.children}
