e94101cb24eb0f28ec328bac94a69ac114b718b8
Blake Wills <blake.wills525@gmail.com>
2016-09-30 03:32:24 -0700

    Install babel-jest, jes, and .babelrc file

---
+++ tests/components/AboutSegment/AboutSegment.test.js
@@ -0,0 +1,55 @@
+// Imports
+// const React = require('react/addons');
+
+// import AboutContainer from '../../src/Components/AboutSegment/AboutContainer.js';
+const AboutContainer = require('../../../src/Components/AboutSegment/AboutContainer.js');
+const AboutDescription = require('../../../src/Components/AboutSegment/AboutDescription.js');
+const AboutInfo = require('../../../src/Components/AboutSegment/AboutInfo.js');
+const AboutMeMaps = require('../../../src/Components/AboutSegment/AboutMeMaps.js');
+const AboutMissionProgram = require('../../../src/Components/AboutSegment/AboutMissionProgram.js');
+const AboutSegment = require('../../../src/Components/AboutSegment/AboutSegment.js');
+
+// describe("AboutSegment component - TestUtils approach", () => {
+//   beforeEach(function() {
+//     let { TestUtils } = React.addons;
+
+//     this.component = TestUtils.renderIntoDocument(<AboutSegment initialName="my first test" />);
+
+//     this.TestUtils = TestUtils;
+//   });
+
+//   it("renders a <div> with class name called AboutSegment", function() { 
+//     let div = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "div");
+//     expect(div.props.children.join('')).toEqual("");
+//   });
+
+//   it("wraps a paragraph with a <div> with a proper class name", function() {
+//     let wrapDiv = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "div");
+
+//     expect(wrapDiv.props.className).toEqual("greeter");
+//   });
+
+//   it("changes greeting person after setting state", function() {
+//     let paragraph = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "p");
+
+//     expect(paragraph.props.children.join('')).toEqual("Hello, my first test!");
+//     this.component.setState({ name: "World" });
+//     expect(paragraph.props.children.join('')).toEqual("Hello, World!");
+//   });
+// });
+
+// Above is an example test to follow: 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
