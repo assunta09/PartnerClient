9989cba1291140a4274a0a7da97c5a18ec4ffafb
Blake Wills <blake.wills525@gmail.com>
2016-09-29 18:03:53 -0700

    Add jest test through npm install

--- tests/components/AboutSegment/AboutSegmenttest.js
+++ tests/components/AboutSegment/AboutSegmenttest.js
@@ -1,18 +1,25 @@
+// Imports
 import React from 'react/addons';
-import Greeter from 'components/Greeter';
+import AboutContainer from 'src/Components/AboutSegment/AboutContainer.js';
+import AboutDescription from 'src/Components/AboutSegment/AboutDescription.js';
+import AboutInfo from 'src/Components/AboutSegment/AboutInfo.js';
+import AboutMeMaps from 'src/Components/AboutSegment/AboutMeMaps.js'; 
+import AboutMissionProgram.js from 'src/Components/AboutSegment/AboutMissionProgram.js';
+import AboutSegment from 'src/Components/AboutSegment/AboutSegment.js';
 
-describe("Greeter component - TestUtils approach", () => {
+
+describe("AboutSegment component - TestUtils approach", () => {
   beforeEach(function() {
     let { TestUtils } = React.addons;
 
-    this.component = TestUtils.renderIntoDocument(<Greeter initialName="my first test" />);
+    this.component = TestUtils.renderIntoDocument(<AboutSegment initialName="my first test" />);
 
     this.TestUtils = TestUtils;
   });
 
-  it("renders a paragraph which greets someone", function() { 
-    let paragraph = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "p");
-    expect(paragraph.props.children.join('')).toEqual("Hello, my first test!");
+  it("renders a <div> with class name called AboutSegment", function() { 
+    let div = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "div");
+    expect(div.props.children.join('')).toEqual("");
   });
 
   it("wraps a paragraph with a <div> with a proper class name", function() {
