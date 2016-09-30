f32f46e617391224590a854637ab0aee336408ae
John Kim <joimkim@gmail.com>
2016-09-30 03:05:59 -0700

    back to top button done

--- src/Components/Footer/FooterSegment.js
+++ src/Components/Footer/FooterSegment.js
@@ -22,7 +22,12 @@ scrollToTop: function() {
   render() {
     return (
       <div className='FooterContainer'>
-      	<div className="FooterTop"><a onClick={this.scrollToTop}>Top</a></div>
+      	<div className="FooterTriangleContainer">
+	    		<div className="FooterTriangle"></div>		
+      	</div>
+      	<div className="FooterTop">
+      			<a id="FooterLink" onClick={this.scrollToTop}>Top</a>
+      	</div>
         <div className='Privacy'>@2016 The Partner | All Rights Reserved</div>
         <div className='FooterLogo'></div>
         <p className='License'>MIT Licensed</p>
