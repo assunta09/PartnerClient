6f91864307c35d1d32ac559a875a187457320d23
John Kim <joimkim@gmail.com>
2016-09-30 13:32:38 -0700

    feature freeze

--- src/Components/BubbleSegment/BubbleSegment.js
+++ src/Components/BubbleSegment/BubbleSegment.js
@@ -3,6 +3,27 @@ import './styles.css';
 import BubbleGraphDescContainer from './BubbleGraphDescContainer.js';
 
 var BubbleSegment = React.createClass({
+	
+	getInitialState: function() {
+		return {
+			exp: '',
+			rev: ''
+		}
+	},
+
+	componentWillReceiveProps: function(nextProps) {
+		var report = nextProps.reports
+		var data = report.reports
+		var exp = data.generalExpensesAbsolutes.total
+		var rev = data.generalRevenueAbsolutes.total
+
+		// console.log(exp)
+		// console.log(rev)	
+		this.setState({
+			exp: exp,
+			rev: rev
+		})
+	},
 
   render() {
     return (
@@ -11,11 +32,13 @@ var BubbleSegment = React.createClass({
 	      	<div className="RevenueBubbleSeg">
 	      		<div className="RevsLabel">
 	      			<h2>Revenue</h2>
+	      			<h3>{'$' + this.state.rev}</h3>
 	      		</div>
 	      	</div>
 	      	<div className="ExpenseBubbleSeg">
 	      	  <div className="ExpsLabel">
 	      			<h2>Expense</h2>
+	      			<h3>{'$' + this.state.exp}</h3>
 	      		</div>
 	      	</div>
       	</div>
