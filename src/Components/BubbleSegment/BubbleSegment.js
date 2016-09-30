8608efeb6ebd8d1ebc8b0a24f784d457bc31e524
John Kim <joimkim@gmail.com>
2016-09-30 13:12:13 -0700

    add rev and exp labels

--- src/Components/BubbleSegment/BubbleSegment.js
+++ src/Components/BubbleSegment/BubbleSegment.js
@@ -7,6 +7,12 @@ var BubbleSegment = React.createClass({
   render() {
     return (
       <div className='BubbleSegment'>
+      	<div className="RevAndExpBubSeg">
+	      	<div className="RevenueBubbleSeg">
+	      	</div>
+	      	<div className="ExpenseBubbleSeg">
+	      	</div>
+      	</div>
         <BubbleGraphDescContainer reports={this.props.reports}/>
       </div>
     );
