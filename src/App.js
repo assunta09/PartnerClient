a22faedc5f0be56234747b773aa5338a330f472c
John Kim <joimkim@gmail.com>
2016-09-30 06:00:46 -0700

    add spinner

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import HeroSegment from './Components/HeroSegment/HeroSegment.js';
 
-class App extends Component {
+var App = React.createClass({
   render() {
     return (
-      <div className="App">
+      <div className="App"> 
         <HeroSegment />
         {this.props.children}
       </div>
     );
   }
-}
-
+});
 export default App;
