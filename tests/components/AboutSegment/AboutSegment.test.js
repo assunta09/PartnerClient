3c9a1bc3d63362c5ef6a1b624775702de3ea630f
Blake Wills <blake.wills525@gmail.com>
2016-09-30 13:22:38 -0700

    Initial test config

--- tests/components/AboutSegment/AboutSegment.test.js
+++ tests/components/AboutSegment/AboutSegment.test.js
@@ -8,37 +8,18 @@ const AboutInfo = require('../../../src/Components/AboutSegment/AboutInfo.js');
 const AboutMeMaps = require('../../../src/Components/AboutSegment/AboutMeMaps.js');
 const AboutMissionProgram = require('../../../src/Components/AboutSegment/AboutMissionProgram.js');
 const AboutSegment = require('../../../src/Components/AboutSegment/AboutSegment.js');
+const renderer = require('react-test-renderer');
 
-// describe("AboutSegment component - TestUtils approach", () => {
-//   beforeEach(function() {
-//     let { TestUtils } = React.addons;
+it('renders correctly', () => {
+  const tree = renderer.create(
+    <AboutSegment reports={this.props.reports}/>
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-//     this.component = TestUtils.renderIntoDocument(<AboutSegment initialName="my first test" />);
 
-//     this.TestUtils = TestUtils;
-//   });
 
-//   it("renders a <div> with class name called AboutSegment", function() { 
-//     let div = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "div");
-//     expect(div.props.children.join('')).toEqual("");
-//   });
 
-//   it("wraps a paragraph with a <div> with a proper class name", function() {
-//     let wrapDiv = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "div");
-
-//     expect(wrapDiv.props.className).toEqual("greeter");
-//   });
-
-//   it("changes greeting person after setting state", function() {
-//     let paragraph = this.TestUtils.findRenderedDOMComponentWithTag(this.component, "p");
-
-//     expect(paragraph.props.children.join('')).toEqual("Hello, my first test!");
-//     this.component.setState({ name: "World" });
-//     expect(paragraph.props.children.join('')).toEqual("Hello, World!");
-//   });
-// });
-
-// Above is an example test to follow: 
 
 
 
